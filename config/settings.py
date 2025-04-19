59229cd1fe9ced8bfa0efb46062a84d290b0be9c
Akos Bujdoso <b00153898@mytudublin.ie>
2025-04-19 13:50:00 +0100

    MariaDB setup with project initialization, super user created manage.py migrated

--- config/settings.py
+++ config/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import pymysql
+pymysql.install_as_MySQLdb()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -76,10 +78,15 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+	'NAME': 'school_supplies',
+	'USER': 'root',
+	'PASSWORD': 'pass',
+	'HOST': 'localhost',
+	'PORT': '3306',
     }
 }
+
 
 
 # Password validation
